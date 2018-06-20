aa1973d55ccf29076574584145a681996cd05d69
M. Scott Ford <scott@corgibytes.com>
2018-06-20 12:07:53 -0400

    Defaults to disable SSL verification

--- lib/carrierwave/storage/ftp/ex_ftptls.rb
+++ lib/carrierwave/storage/ftp/ex_ftptls.rb
@@ -5,7 +5,11 @@ class ExFTPTLS < Net::FTP
   include ExFTPMixin
 
   def self.new(host = nil, options = {})
-    options[:ssl] = true unless options[:ssl]
+    options[:ssl] = default_ssl_options unless options[:ssl]
     super(host, options)
+  end
+
+  def self.default_ssl_options
+    { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   end
 end
